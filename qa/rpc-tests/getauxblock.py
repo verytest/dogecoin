namecoin/namecore

Disable getblocktemplate.
Comment out and disable the "getblocktemplate" RPC command.  It makes no
sense for merge-mining, as getauxblock should be used instead.
Furthermore, we are going to force all blocks to be merge-mined in the
future.  Then GBT makes even less sense.

qa/rpc-tests/getauxblock.py
@@ -18,20 +18,11 @@ class GetAuxBlockTest (BitcoinTestFramework):
     # Generate a block so that we are not "downloading blocks".
     self.nodes[0].generate (1)
 
-    # Compare basic data of getauxblock to getblocktemplate.
-    auxblock = self.nodes[0].getauxblock ()
-    blocktemplate = self.nodes[0].getblocktemplate ()
-    assert_equal (auxblock['coinbasevalue'], blocktemplate['coinbasevalue'])
-    assert_equal (auxblock['bits'], blocktemplate['bits'])
-    assert_equal (auxblock['height'], blocktemplate['height'])
-    assert_equal (auxblock['previousblockhash'], blocktemplate['previousblockhash'])
-
-    # Compare target and take byte order into account.
-    target = auxblock['_target']
-    reversedTarget = auxpow.reverseHex (target)
-    assert_equal (reversedTarget, blocktemplate['target'])
+    # We used to compare to getblocktemplate, but this call is gone
+    # now completely for merge-mining.
 
     # Verify data that can be found in another way.
+    auxblock = self.nodes[0].getauxblock ()
     assert_equal (auxblock['chainid'], 1)
     assert_equal (auxblock['height'], self.nodes[0].getblockcount () + 1)
     assert_equal (auxblock['previousblockhash'], self.nodes[0].getblockhash (auxblock['height'] - 1))
@@ -67,8 +58,7 @@ class GetAuxBlockTest (BitcoinTestFramework):
     self.sync_all ()
     assert_equal (self.nodes[1].getrawmempool (), [txid])
     auxblock = self.nodes[0].getauxblock ()
-    blocktemplate = self.nodes[0].getblocktemplate ()
-    target = blocktemplate['target']
+    target = auxpow.reverseHex (auxblock['_target'])
 
     # Compute invalid auxpow.
     apow = auxpow.computeAuxpow (auxblock['hash'], target, False)

verytest/dogecoin

Cherry-picked from namecore.
Conflicts:
  qa/pull-tester/rpc-tests.py
  qa/rpc-tests/getblocktemplate_longpoll.py
  qa/rpc-tests/getblocktemplate_proposals.py
  src/rpc/server.cpp
  src/rpc/server.h
  src/rpcmining.cpp

qa/rpc-tests/getauxblock.py
@@ -22,21 +22,13 @@ class GetAuxBlockTest (BitcoinTestFramework):
     # Generate a block so that we are not "downloading blocks".
     self.nodes[0].generate (1)
 
-    # Compare basic data of getauxblock to getblocktemplate.
-    auxblock = self.nodes[0].getauxblock ()
-    blocktemplate = self.nodes[0].getblocktemplate ()
-    assert_equal (auxblock['coinbasevalue'], blocktemplate['coinbasevalue'])
-    assert_equal (auxblock['bits'], blocktemplate['bits'])
-    assert_equal (auxblock['height'], blocktemplate['height'])
-    assert_equal (auxblock['previousblockhash'], blocktemplate['previousblockhash'])
-
-    # Compare target and take byte order into account.
-    target = auxblock['target']
-    reversedTarget = auxpow.reverseHex (target)
-    assert_equal (reversedTarget, blocktemplate['target'])
+    # We used to compare to getblocktemplate, but this call is gone
+    # now completely for merge-mining.
 
     # Verify data that can be found in another way.
+    auxblock = self.nodes[0].getauxblock ()
     assert_equal (auxblock['chainid'], 98)
+
     assert_equal (auxblock['height'], self.nodes[0].getblockcount () + 1)
     assert_equal (auxblock['previousblockhash'], self.nodes[0].getblockhash (auxblock['height'] - 1))
 
@@ -71,8 +63,7 @@ class GetAuxBlockTest (BitcoinTestFramework):
     self.sync_all ()
     assert_equal (self.nodes[1].getrawmempool (), [txid])
     auxblock = self.nodes[0].getauxblock ()
-    blocktemplate = self.nodes[0].getblocktemplate ()
-    target = blocktemplate['target']
+    target = auxpow.reverseHex (auxblock['target'])
 
     # Compute invalid auxpow.
     apow = scrypt_auxpow.computeAuxpowWithChainId (auxblock['hash'], target, "01", False)