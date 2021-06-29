1edb76a44bb90fc3c80b05851a71f8c9974323e7
Patrick Lodder <patricklodder@users.noreply.github.com>
2021-06-29 20:13:49 +0200

    WIP test relay

--- qa/rpc-tests/mempool_spam.py
+++ qa/rpc-tests/mempool_spam.py
@@ -15,7 +15,9 @@ class MempoolSpamTest(BitcoinTestFramework):
 
     def setup_network(self):
         self.nodes = []
-        self.nodes.append(start_node(0, self.options.tmpdir, ["-minrelaytxfee=0.001", "-maxmempool=5", "-spendzeroconfchange=0", "-debug"]))
+        for _ in range(self.num_nodes):
+            self.new_node()
+        connect_nodes(self.nodes[0], 1)
         self.is_network_split = False
         self.sync_all()
         self.relayfee = self.nodes[0].getnetworkinfo()['relayfee']
@@ -23,17 +25,23 @@ class MempoolSpamTest(BitcoinTestFramework):
     def __init__(self):
         super().__init__()
         self.setup_clean_chain = True
-        self.num_nodes = 1
+        self.num_nodes = 2
 
         self.txouts = gen_return_txouts()
+
+    def new_node(self):
+        self.nodes.append(start_node(len(self.nodes), self.options.tmpdir, ["-minrelaytxfee=0.001", "-maxmempool=8", "-spendzeroconfchange=0", "-debug"]))
 
     def run_test(self):
         txids = []
         utxos = create_confirmed_utxos(self.relayfee, self.nodes[0], 181)
 
+        self.sync_all()
+
         for i in range (2):
             txids.append([])
             txids[i] = create_lots_of_big_transactions(self.nodes[0], self.txouts, utxos[30*i:30*i+30], 30, self.relayfee * i)
+            self.sync_all()
 
         #create a mempool tx with fee > self.txouts
         us0 = utxos.pop()
@@ -49,10 +57,21 @@ class MempoolSpamTest(BitcoinTestFramework):
         for i in range (2,4):
             txids.append([])
             txids[i] = create_lots_of_big_transactions(self.nodes[0], self.txouts, utxos[30*i:30*i+30], 30, self.relayfee*i)
+            self.sync_all()
 
         # our transaction should still live in the mempool
-        assert(txid in self.nodes[0].getrawmempool())
-        assert(self.nodes[0].getmempoolinfo()['size'] < sum(1 for _ in it.chain.from_iterable(txids)))
+        assert(self.nodes[0].getmempoolinfo()['size'] == self.nodes[1].getmempoolinfo()['size'])
+        for i in range(self.num_nodes):
+            assert(txid in self.nodes[i].getrawmempool())
+            assert(self.nodes[1].getmempoolinfo()['size'] < sum(1 for _ in it.chain.from_iterable(txids)))
+
+        # mine a block on node 1
+        self.nodes[1].generate(1)
+        self.sync_all()
+
+        # our transaction should be mined
+        for i in range(self.num_nodes):
+            assert(txid not in self.nodes[i].getrawmempool())
 
 if __name__ == '__main__':
     MempoolSpamTest().main()
