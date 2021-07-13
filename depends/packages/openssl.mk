185332ad05a98693a48048d70e3ac26bd94e2ca3
Patrick Lodder <patricklodder@users.noreply.github.com>
2021-07-13 22:28:24 +0200

    API level 1.0.1

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -8,6 +8,7 @@ $(package)_sha256_hash=892a0875b9872acd04a9fde79b1f943075d5ea162415de3047c327df3
 define $(package)_set_vars
 $(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
 $(package)_config_opts=--prefix=$(host_prefix) --openssldir=$(host_prefix)/etc/openssl
+$(package)_config_opts+=--api=1.0.1
 $(package)_config_opts+=no-camellia
 $(package)_config_opts+=no-capieng
 $(package)_config_opts+=no-cast
