b5c1d3deed2a0b7b91365d870ef0943effe9b324
Patrick Lodder <patricklodder@users.noreply.github.com>
2021-09-27 23:45:25 +0200

    wip: fix xkb path export

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -98,7 +98,7 @@ $(package)_config_opts_linux += -no-opengl
 $(package)_config_opts_arm_linux  = -platform linux-g++ -xplatform $(host)
 $(package)_config_opts_i686_linux  = -xplatform linux-g++-32
 $(package)_config_opts_mingw32  = -no-opengl -xplatform win32-g++ -device-option CROSS_COMPILE="$(host)-"
-$(package)_build_env  = QT_RCC_TEST=1
+$(package)_build_env  = QT_RCC_TEST=1 QT_XKB_CONFIG_ROOT=auto
 endef
 
 define $(package)_fetch_cmds
