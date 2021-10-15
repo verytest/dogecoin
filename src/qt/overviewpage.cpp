8b1463406739f4a3e706b06889d842d90496f96f
Patrick Lodder <patricklodder@users.noreply.github.com>
2021-10-15 21:27:10 +0200

    qt: display fuller version in logo
    Removes "1.14" that was embedded in the logo image on the overview
    screen and replaces it with a dynamically generated version.
    
    Build number will not be displayed as long as it is 0, like with
    the subversion sent to peers.

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -7,6 +7,7 @@
 
 #include "bitcoinunits.h"
 #include "clientmodel.h"
+#include "clientversion.h"
 #include "guiconstants.h"
 #include "guiutil.h"
 #include "optionsmodel.h"
@@ -128,6 +129,11 @@ OverviewPage::OverviewPage(const PlatformStyle *platformStyle, QWidget *parent) 
     icon.addPixmap(icon.pixmap(QSize(64,64), QIcon::Normal), QIcon::Disabled); // also set the disabled icon because we are using a disabled QPushButton to work around missing HiDPI support of QLabel (https://bugreports.qt.io/browse/QTBUG-42503)
     ui->labelTransactionsStatus->setIcon(icon);
     ui->labelWalletStatus->setIcon(icon);
+
+    //style and set the current version
+    ui->label_wallet_version_overlay->setStyleSheet("QLabel { color: red; font-size: 16pt; font-weight: 600; padding-top: 190px; }");
+    ui->label_wallet_version_overlay->setText(QString::fromStdString(FormatVersion(CLIENT_VERSION)));
+
 
     // Set tip of the day
     UpdateTip();
